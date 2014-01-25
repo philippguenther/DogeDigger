eabddaf09458dba2db1f78e47e94a1ccd080780e
philipp <pguenther.imap@gmail.com>
2014-01-26 00:21:37 +0100

    introducing Level and Boxes

--- src/Config.java
+++ src/Config.java
@@ -3,11 +3,23 @@ public class Config {
 	private static int windowWidth = 800;
 	private static int windowHeight = 600;
 	
+	private static float boxSize = 100f;
+	
+	private static float colorTolerance = 0.2f;
+	
 	public static int getWindowWidth() {
 		return Config.windowWidth;
 	}
 	
 	public static int getWindowHeight() {
 		return Config.windowHeight;
+	}
+	
+	public static float getBoxSize() {
+		return Config.boxSize;
+	}
+	
+	public static float getColorTolerance() {
+		return Config.colorTolerance;
 	}
 }
--- src/SwapBoxes.java
+++ src/SwapBoxes.java
@@ -1,52 +1,80 @@
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 
-public class SwapBoxes {
+enum State {
+	INPUT,
+	ANIMATING
+}
 
-	public static void main(String[] args) {
+public class SwapBoxes {
+	private long lastFrame;
+	private Level currentLevel;
+	public State state = State.INPUT;
+
+	public void start() {
 		try {
 			Display.setDisplayMode(new DisplayMode(800, 600));
 			Display.create();
 		} catch(LWJGLException e) {
 			e.printStackTrace();
-			System.exit(0);
+			System.exit(1);
+		}
+		
+		try {
+			Mouse.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
 		
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
-		GL11.glOrtho(0, Config.getWindowWidth(), 0, Config.getWindowHeight(), 1, -1);
+		GL11.glOrtho(0, Config.getWindowWidth() / Config.getBoxSize(), Config.getWindowHeight() / Config.getBoxSize(), 0, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		
+		this.currentLevel = LevelFactory.getRandomLevel();
+		
 		while(!Display.isCloseRequested()) {
-			processInput();
-			render();
+			int delta = this.getDelta();
+			
+			input();
+			
+			// Clear the screen and depth buffer
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			this.currentLevel.render(delta);
+			
 			Display.update();
+			Display.sync(60);
 		}
 		
 		Display.destroy();
 	}
 	
-	private static void processInput() {
-		
+	private void input() {
+		if (this.state == State.INPUT)
+			this.currentLevel.input();
+	}
+
+	
+	private long getTime() {
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
 	}
 	
-	private static void render() {
-		// Clear the screen and depth buffer
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		
-		// set the color of the quad (R,G,B,A)
-		GL11.glColor3f(0.5f,0.5f,1.0f);
-		
-		// draw quad
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(100,100);
-		GL11.glVertex2f(100+200,100);
-		GL11.glVertex2f(100+200,100+200);
-		GL11.glVertex2f(100,100+200);
-		GL11.glEnd();
+	private int getDelta() {
+		long time = getTime();
+		int delta = (int) (time - this.lastFrame);
+		this.lastFrame = time;
+		return delta;
+	}
+	
+	public static void main(String[] args) {
+		SwapBoxes game = new SwapBoxes();
+		game.start();
 	}
 
 }
