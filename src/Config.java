3d258b46b6efe837a989714c463ffb86712656eb
philipp <pguenther.imap@gmail.com>
2014-01-25 21:13:00 +0100

    initial commit

---
+++ src/Config.java
@@ -0,0 +1,13 @@
+
+public class Config {
+	private static int windowWidth = 800;
+	private static int windowHeight = 600;
+	
+	public static int getWindowWidth() {
+		return Config.windowWidth;
+	}
+	
+	public static int getWindowHeight() {
+		return Config.windowHeight;
+	}
+}
